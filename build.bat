874614170480d15db8911c0098b253d2108ecc49
brinkqiang <brink.qiang@gmail.com>
2021-11-18 14:49:10 +0800

    ADD

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -DLEVELDB_BUILD_TESTS=ON ..
 cmake --build . --config relwithdebinfo
 popd
 
